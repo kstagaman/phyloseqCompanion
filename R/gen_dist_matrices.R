f9a526df0695b63632b5672886c3f8f9261e243d
Keaton Stagaman <stagamak@oregonstate.edu>
2022-03-22 14:27:42 -0700

    useful (hopefully) updates to gen.dist.matrices()

--- R/gen_dist_matrices.R
+++ R/gen_dist_matrices.R
@@ -18,6 +18,7 @@ gen.dist.matrices <- function(
   methods = c(
     "all",
     "taxonomic",
+    "taxonomic with Aitchison",
     "phylogenetic",
     "Bray-Curtis",
     "Canberra",
@@ -26,58 +27,128 @@ gen.dist.matrices <- function(
     "W Unifrac",
     "0.5 Unifrac",
     "U Unifrac"
-    ),
+  ),
   cores = 1,
   verbose = TRUE,
   clr_ps = NULL
 ){
-  if (methods %in% c("all", "taxonomic", "Aitchison") & is.null(clr_ps)) {
+  method.ids <-  c(
+    "bray.curtis", "canberra", "sorensen", "aitchison",
+    "w.unifrac", "h.unifrac", "u.unifrac"
+  )
+  method.choices <- tolower(methods) %>%
+    str_replace_all("ø", "o") %>%
+    str_replace_all("weighted", "w") %>%
+    str_replace_all("0.5|half-weighted|half", "h") %>%
+    str_replace_all("unweighted", "u") %>%
+    str_replace_all("[ -]", ".")
+  aitch.match <- method.choices == "taxonomic.with.aitchison"
+  if (any(aitch.match)) {
+    method.choices[which(aitch.match)] <- "tax.with.aitch"
+  }
+  arg.res <- list(
+    all            = list(Req.clrPS = TRUE,  Methods = method.ids),
+    taxonomic      = list(Req.clrPS = FALSE, Methods = method.ids[1:3]),
+    tax.with.aitch = list(Req.clrPS = TRUE,  Methods = method.ids[1:4]),
+    phylogenetic   = list(Req.clrPS = FALSE, Methods = method.ids[5:7]),
+    bray.curtis    = list(
+      Req.clrPS = FALSE,
+      Methods = "bray.curtis",
+      Name = "Bray-Curtis"
+    ),
+    canberra       = list(
+      Req.clrPS = FALSE,
+      Methods = "canberra",
+      Name = "Canberra"
+    ),
+    sorensen       = list(
+      Req.clrPS = FALSE,
+      Methods = "sorensen",
+      Name = "Sørensen"
+    ),
+    aitchison      = list(
+      Req.clrPS = TRUE,
+      Methods = "aitchison",
+      Name = "Aitchison"
+    ),
+    w.unifrac      = list(
+      Req.clrPS = FALSE,
+      Methods = "w.unifrac",
+      Name = "Weighted UniFrac"
+    ),
+    h.unifrac      = list(
+      Req.clrPS = FALSE,
+      Methods = "h.unifrac",
+      Name = "Half-weighted UniFrac"
+    ),
+    u.unifrac      = list(
+      Req.clrPS = FALSE,
+      Methods = "u.unifrac",
+      Name = "Unweighted UniFrac"
+    )
+  )
+  get.arg.meta <- function(choices, name) {
+    sapply(choices, function(choice) { arg.res[[choice]][[name]] })
+  }
+  req.clrPS <- any(get.arg.meta(method.choices, "Req.clrPS")) &
+    is.null(clr_ps)
+  if (req.clrPS) {
     stop(
-      '`clr_ps` must be supplied if methods that include "Aitchison" are called ("all", "taxonomic", "Aitchison")'
+      '`clr_ps` must be supplied if methods that include "Aitchison" are called ("all", "taxonomic with Aitchison", "Aitchison")'
     )
   }
-  dists.dt <- data.table(
-    Name = c("Bray-Curtis", "Canberra", "Sørensen", "Aitchison", "W Unifrac", "0.5 Unifrac", "U Unifrac"),
-    Method = c("bray", "canberra", "bray", "euclidean", NA, NA, NA),
+  dists.dt0 <- data.table(
+    ID = c(
+      "bray.curtis",
+      "canberra",
+      "sorensen",
+      "aitchison",
+      "w.unifrac",
+      "h.unifrac",
+      "u.unifrac"
+    ),
+    Arg = c("bray", "canberra", "bray", "euclidean", NA, NA, NA),
     Param = c(0, NA, 1, NA, 1, 0.5, 0)
   )
-  if (methods[1] == "taxonomic") {
-    dists.dt <- dists.dt[!is.na(Method)]
-  } else if (methods[1] == "phylogenetic") {
-    dists.dt <- dists.dt[is.na(Method)]
-  } else if (methods[1] != "all") {
-    dists.dt <- dists.dt[Name %in% methods]
-  }
-  dist.names <- copy(dists.dt$Name) %>% magrittr::set_names(., .)
-  names(dist.names) <- dist.names
-  setkey(dists.dt, Name)
+  setkey(dists.dt0, ID)
+  dists.dt <- dists.dt0[
+    unique(unname(unlist(get.arg.meta(method.choices, "Methods"))))
+    ]
+  setkey(dists.dt, ID)
+  dist.ids <- copy(dists.dt$ID) %>% set_names(., get.arg.meta(., "Name"))
   unit.tbl <- otu.matrix(ps)
-  if ("Aitchison" %in% dist.names) {
+  if ("aitchison" %in% dist.ids) {
     clr.tbl <- otu.matrix(clr_ps)
   }
 
   if (cores > 1) {
-    cl <- makeCluster(cores, type = "FORK")
-    registerDoParallel(cl, cores)
+    usable.cores <- min(c(cores, length(dist.ids)))
+    cl <- makeCluster(usable.cores, type = "FORK")
+    registerDoParallel(cl, usable.cores)
     dist.list <- foreach(
-      n = dist.names,
-      .final = function(x) setNames(x, names(dist.names)),
+      n = dist.ids,
+      .final = function(x) setNames(x, names(dist.ids)),
       .verbose = verbose
     ) %dopar% {
-      if (is.na(dists.dt[n, "Method"])) {
+      if (is.na(dists.dt[n, "Arg"])) {
         tree <- phy_tree(ps)
-        dist <- GUniFrac(unit.tbl, tree, alpha = dists.dt[n]$Param)$unifracs[, , 1] %>% as.dist()
+        dist <- GUniFrac(
+          unit.tbl,
+          tree,
+          alpha = dists.dt[n]$Param
+          )$unifracs[, , 1] %>%
+          as.dist()
       } else {
         if (is.na(dists.dt[n, "Param"])) {
-          if (n == "Aitchison") {
-            dist <- vegan::vegdist(clr.tbl, method = dists.dt[n]$Method)
+          if (n == "aitchison") {
+            dist <- vegan::vegdist(clr.tbl, method = dists.dt[n]$Arg)
           } else {
-            dist <- vegan::vegdist(unit.tbl, method = dists.dt[n]$Method)
+            dist <- vegan::vegdist(unit.tbl, method = dists.dt[n]$Arg)
           }
         } else {
           dist <- vegan::vegdist(
             unit.tbl,
-            method = dists.dt[n]$Method,
+            method = dists.dt[n]$Arg,
             binary = dists.dt[n]$Param
           )
         }
@@ -86,21 +157,26 @@ gen.dist.matrices <- function(
     }
     stopCluster(cl)
   } else {
-    dist.list <- lapply(dist.names, function(n) {
-      if (is.na(dists.dt[n, "Method"])) {
+    dist.list <- lapply(dist.ids, function(n) {
+      if (is.na(dists.dt[n, "Arg"])) {
         tree <- phy_tree(ps)
-        dist <- GUniFrac(unit.tbl, tree, alpha = dists.dt[n]$Param)$unifracs[, , 1] %>% as.dist()
+        dist <- GUniFrac(
+          unit.tbl,
+          tree,
+          alpha = dists.dt[n]$Param
+          )$unifracs[, , 1] %>%
+          as.dist()
       } else {
         if (is.na(dists.dt[n, "Param"])) {
           if (n == "Aitchison") {
-            dist <- vegan::vegdist(clr.tbl, method = dists.dt[n]$Method)
+            dist <- vegan::vegdist(clr.tbl, method = dists.dt[n]$Arg)
           } else {
-            dist <- vegan::vegdist(unit.tbl, method = dists.dt[n]$Method)
+            dist <- vegan::vegdist(unit.tbl, method = dists.dt[n]$Arg)
           }
         } else {
           dist <- vegan::vegdist(
             unit.tbl,
-            method = dists.dt[n]$Method,
+            method = dists.dt[n]$Arg,
             binary = dists.dt[n]$Param
           )
         }
